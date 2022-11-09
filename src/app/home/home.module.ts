5156e47df6694d39f3cdc4f7a6dcc8e1ea63a781
Félix Gómez Enríquez <felixmihmo@gmail.com>
2022-11-09 13:08:17 +0100

    1º subida de archivos inicial

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -12,6 +14,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpClientModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
